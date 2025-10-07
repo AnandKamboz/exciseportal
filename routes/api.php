be2677103a9bf13bb44613d24578f345d95b9e7f
Anand Kamboj <anandkamboj01@gmail.com>
2025-10-06 20:39:45 -0700

    fixed error

--- routes/api.php
+++ routes/api.php
@@ -13,13 +13,17 @@ Route::get('/test', function () {
     ]);
 });
 
-
 Route::post('/send-otp', [AuthController::class, 'sendOtp']);
 Route::post('/verify-otp', [AuthController::class, 'verifyOtp']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/complaints/step-first-x9f2kq', [ComplainantController::class, 'storeFirstStep']);
-    Route::post('/complaints/step-second-b7p4mz', [ComplainantController::class, 'storeSecondStep']);
-    Route::post('/complaints/final-step-t6y9dw', [ComplainantController::class, 'store']);
-    Route::get('/districts', [DistrictController::class, 'getAllDistricts']);
+    // Route::post('/complaints/step-first', [ComplainantController::class, 'storeFirstStep']);
+
+
+
+    // Route::post('/complaints/step-first-x9f2kq', [ComplainantController::class, 'storeFirstStep']);
+    // Route::post('/complaints/step-second-b7p4mz', [ComplainantController::class, 'storeSecondStep']);
+    // Route::post('/complaints/final-step-t6y9dw', [ComplainantController::class, 'store']);
+    // Route::get('/districts', [DistrictController::class, 'getAllDistricts']);
 });
+  Route::post('/complaints/step-first', [ComplainantController::class, 'storeFirstStep']);
