1fbd1bf248df43e55ee323df31b16f374b51b636
Anand Kamboj <anandkamboj01@gmail.com>
2025-12-09 11:12:09 +0530

    fixed error

--- database/migrations/2025_12_09_050808_create_jc_action_logs.php
+++ database/migrations/2025_12_09_050808_create_jc_action_logs.php
@@ -11,10 +11,6 @@ return new class extends Migration
      */
     public function up(): void
     {
-        // Schema::create('jc_action_logs', function (Blueprint $table) {
-        //     $table->id();
-        //     $table->timestamps();
-        // });
         Schema::create('jc_action_logs', function (Blueprint $table) {
             $table->id();
 
