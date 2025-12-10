94b5e5e89a0a9861dad78e44c2bf72f35077eeff
Anand Kamboj <anandkamboj01@gmail.com>
2025-12-10 16:47:59 +0530

    fixed error

--- database/migrations/2025_09_29_100000_create_districts_table.php
+++ database/migrations/2025_09_29_100000_create_districts_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('districts', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('name_hi');
             $table->string('code');
             $table->timestamps();
         });
