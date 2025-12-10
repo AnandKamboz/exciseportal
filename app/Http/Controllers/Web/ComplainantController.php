69bb3c6e45b5758f9661c0cec6dc727809b43ba5
Anand Kamboj <anandkamboj01@gmail.com>
2025-12-10 10:39:36 +0530

    FIXED ERROR

--- app/Http/Controllers/Web/ComplainantController.php
+++ app/Http/Controllers/Web/ComplainantController.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 use Illuminate\Validation\Rule;
+use App\Models\DetcAction;
 
 class ComplainantController extends Controller
 {
